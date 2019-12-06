5a87e9e39be226e3d566f08adf53e6956cb806a2
James Ginns <starvagrant@yahoo.com>
2019-12-06 16:31:28 -0600

    Use valid info to update site dynamically.
    Use validated form input to update the "Awaiting
    information" to display shuttle problems and to
    reverse already displayed shuttle items when correct
    info is given.

--- script.js
+++ script.js
@@ -19,13 +19,56 @@ function validateForm() {
     }
 }
 
+function insertNames() {
+    let pilotName = document.getElementById('pilotName').value;
+    let copilotName = document.getElementById('copilotName').value;
+    let pilotStatus = document.getElementById('pilotStatus');
+    let copilotStatus = document.getElementById('copilotStatus');
+    pilotStatus.innerHTML = `${pilotName}`;
+    copilotStatus.innerHTML = `${copilotName}`;
+}
+
+function alertShuttleNeeds() {
+    let fuelLevel = document.getElementById('fuelLevel').value;
+    let cargoMass = document.getElementById('cargoMass').value;
+    let fuelGood = true;
+    let cargoGood = true;
+    if (fuelLevel <= 10000) fuelGood = false;
+    if (cargoMass >= 10000) cargoGood = false;
+    let faultyItems = document.getElementById("faultyItems");
+    let launchStatus = document.getElementById("launchStatus");
+    if (!fuelGood || !cargoGood) {
+        faultyItems.style.visibility = "visible";
+        launchStatus.style.color = "red";
+        launchStatus.innerHTML = "Shuttle not ready for launch";
+        let fuelStatus = document.getElementById('fuelStatus');
+        let cargoStatus = document.getElementById('cargoStatus');
+        if (!fuelGood) {
+            fuelStatus.innerHTML = "There is not enough fuel for the journey";
+        } else {
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+        }
+        if (!cargoGood) {
+            cargoStatus.innerHTML = "There is too much mass for takeoff.";
+        } else {
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+        }
+    } else {
+        faultyItems.style.visibility = "hidden";
+        launchStatus.style.color = "";
+        launchStatus.innerHTML = "Awaiting Information Before Launch";
+    }
+}
+
 window.addEventListener("load", function() {
     validateForm()
     let form = document.querySelector('form');
     form.addEventListener("submit", function(event) {
         event.preventDefault();
         validateForm();
-        console.log('form validates!');
+        insertNames();
+        alertShuttleNeeds();
+        console.log('program complete!');
     });
 });
 
