3c185586ac8e5a1363973aa049dcce851235e656
James Ginns <starvagrant@yahoo.com>
2019-12-06 15:22:26 -0600

    Add form validation

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-// Write your JavaScript code here!
+function validateForm() {
+    let form = document.querySelector('form');
+    for(i=0; i<4;i++) {
+        if (form[i].value === "") {
+            alert('Fill in All Fields');
+            return;
+        }
+    }
+
+    let fuelLevel = document.getElementById('fuelLevel');
+    if (!isNaN(Number(fuelLevel))) {
+        alert('Set fuel Level to a Number');
+        return;
+    }
+    let cargoMass = document.getElementById('cargoMass');
+    if (!isNaN(Number(fuelLevel))) {
+        alert('Set cargoMass to a Number');
+        return;
+    }
+}
+
+window.addEventListener("load", function() {
+    validateForm()
+    let form = document.querySelector('form');
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        validateForm();
+        console.log('form validates!');
+    });
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
